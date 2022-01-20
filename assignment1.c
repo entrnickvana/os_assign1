b957079f71006a0e9c8ee346bee518d2db8208d5
entrnickvana <nick.jwl123@gmail.com>
2022-01-19 22:09:52 -0700

    init

---
+++ assignment1.c
@@ -0,0 +1,28 @@
+
+
+#include "assignment1.h"
+
+uint64_t byte_sort(uint64_t arg){
+  printf("byte_sort bitch!");
+  return 0;
+}
+
+uint64_t nibble_sort(uint64_t arg){
+  printf("nibble_sort bitch!");
+  return 0;
+}
+
+struct elt* str_to_list(const char* str){
+  printf("str_to_list bitch!");
+  return 0;
+}
+
+void convert(enum format_t mode, uint64_t value){
+  printf("convert bitch!");
+  return;
+}
+
+void draw_u(void){
+  printf("draw_u bitch!");  
+  return;
+}
---
+++ nick_test.c
@@ -0,0 +1,9 @@
+
+#include "assignment1.h"
+
+int main(void){
+
+  byte_sort(0);
+  nibble_sort(0);
+  
+}
